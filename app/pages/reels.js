22692e27386bf70c9f9baf6107c0370ccb7a07d6
dishamane23 <manedisha67@gmail.com>
2025-03-03 11:12:06 +0530

    moments page

---
+++ app/pages/reels.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import ReelsComponent from '../components/ReelsComponent';
+import Header  from '../components/header';
+import Footer from '../components/footer';
+const Reels = () => {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
+  return (
+    <View
+      style={{
+        width: windowWidth,
+        height: windowHeight,
+        backgroundColor: 'white',
+        position: 'relative',
+        backgroundColor: 'black',
+      }}>
+      {/* <View
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          zIndex: 1,
+          padding: 10,
+        }}>
+        <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+          Reels
+        </Text>
+        <Feather name="camera" style={{fontSize: 25, color: 'white'}} />
+      </View> */}
+      <Header isTransparent={true} />
+      <ReelsComponent />
+      <Footer/>
+    </View>
+  );
+};
+
+export default Reels;
